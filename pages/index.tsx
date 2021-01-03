e4ca355e0237302655013a93eb584af7942c43fd
Brian Sokas <brian@ethermed.ai>
2021-01-03 13:42:50 -0500

    First push. Setup next.js app and swap out index.js for index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css'
+
+function PrintoxWeb(){
+    return (
+        <div className={styles.container}>
+            <h1>Welcome to Printox!</h1>
+            <p>Printing, for the whole herd</p>
+        </div>
+    )
+}
+
+export default PrintoxWeb
